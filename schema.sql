c6f0041e1b7202895e1515c95538f411a10ef819
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2022-02-18 22:44:24 +0200

    Create owners & species tables, delete species column, and add foreign keys

--- schema.sql
+++ schema.sql
@@ -12,3 +12,34 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(30);
+
+DROP TABLE IF EXISTS owners;
+CREATE TABLE owners (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(100),
+  age INT,
+  PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS species;
+CREATE TABLE species (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(100),
+  PRIMARY KEY(id)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals
+  ADD COLUMN species_id INT,
+  ADD CONSTRAINT fk_species_id
+    FOREIGN KEY (species_id)
+    REFERENCES species(id)
+    ON DELETE CASCADE;
+
+ALTER TABLE animals
+  ADD COLUMN owner_id INT,
+  ADD CONSTRAINT fk_owner_id
+    FOREIGN KEY (owner_id)
+    REFERENCES owners(id)
+    ON DELETE CASCADE;
